362b22ca984cf8661be11a035170848a9b1e3a30
Taner ERKAN <Tanererkan06@users.noreply.github.com>
2019-11-19 05:03:13 +0300

    Update HomeScreen.js

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react'
 import {Button,Dimensions,
-  TextInput,View,Text, Alert} from 'react-native';
+  TextInput,View,Text, Alert,StyleSheet} from 'react-native';
 import CheckBox from 'react-native-checkbox';
 import Buton from '../components/Buton';
-
+import AutoTags from 'react-native-tag-autocomplete';
 
 const H = Dimensions.get('screen').height;
 const W = Dimensions.get('screen').width;
 
 
-export default class HomeScreen extends Component {
+class HomeScreen extends Component {
   static navigationOptions = {
     header: null,
     };
@@ -17,12 +17,36 @@ export default class HomeScreen extends Component {
     super(props);
   
   this.state = {
-  
+    tagsSelected: [],
+    diller:null,
     Hizmet:false,
     Tercuman:false
   }
   
   }
+  handleDelete = index => {
+    //tag deleted, remove from our tags array
+    let tagsSelected = this.state.tagsSelected;
+    tagsSelected.splice(index, 1);
+    this.setState({ tagsSelected });
+  }
+
+  handleAddition = dil => {
+    //suggestion clicked, push it to our tags array
+    this.setState({ tagsSelected: this.state.tagsSelected.concat([dil]) });
+  }
+
+  componentDidMount() {
+    return fetch('http://192.168.8.137/User_Project/diller.php')
+      .then((response) => response.json())
+      .then((responseJson) => {
+      this.setState({diller:responseJson})
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
   Onay = () => {
         if (this.state.Hizmet==false&&this.state.Tercuman==false) {
           Alert.alert('Seçiminizi Yapın')
@@ -35,10 +59,16 @@ export default class HomeScreen extends Component {
   render() {
     return (
       <View style={{ flex: 1,marginTop:H/18 }}>
-         <View style={{ alignItems:'center',justifyContent:'center' }}>
+         <View style={styles.autocompleteContainer}>
          <Text style={{fontSize: 28, color:"#000",paddingBottom:20}}>Bildiğiniz Dilleri Giriniz</Text>
-      <TextInput style={{borderWidth:1,borderColor:"#707070",borderRadius:4,height:40,width:W/1.5}}/>
-         </View>
+          <AutoTags
+            suggestions={this.state.diller}
+            tagsSelected={this.state.tagsSelected}
+            handleAddition={this.handleAddition}
+            handleDelete={this.handleDelete}
+          />
+        </View>
+
      
       <View style={{alignItems:'flex-start',}}>
 
@@ -73,18 +103,50 @@ export default class HomeScreen extends Component {
           onPress={this.Onay}
         />
 
-        <Buton title="Giriş Yap"
+        <Buton title="Oturum Aç"
           style={{width:W/1.5}}
           onPress={()=> this.props.navigation.navigate('Giris')}
         />
 
-         <Buton title="WebRtc"
-          style={{width:W/1.5}}
-          onPress={()=> this.props.navigation.navigate('Webrtc')}
-        />
+        
     </View>
         
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  autocompleteContainer: {
+    alignItems:'center',
+    justifyContent:'center',
+    paddingTop: 0,
+    zIndex: 1,
+    width: "100%",
+    paddingHorizontal: 8,
+    marginBottom:40,
+  },
+  input: {maxHeight: 40},
+  inputContainer: {
+    display: "flex",
+    flexShrink: 0,
+    flexGrow: 0,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    alignItems: "center",
+    borderBottomWidth: 1,
+    borderColor: "#c7c6c1",
+    paddingVertical: 13,
+    paddingLeft: 12,
+    paddingRight: "5%",
+    width: "100%",
+    justifyContent: "flex-start",
+  },
+  plus: {
+    position: "absolute",
+    left: 15,
+    top: 10,
+  },
+});
+
+export default HomeScreen;
