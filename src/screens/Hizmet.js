e8b041472ded672d58d0d0e178d982fe75fe306e
Taner ERKAN <Tanererkan06@users.noreply.github.com>
2019-11-19 05:02:30 +0300

    Update Hizmet.js

--- src/screens/Hizmet.js
+++ src/screens/Hizmet.js
@@ -6,7 +6,7 @@ import RNFetchBlob from 'rn-fetch-blob';
 import  TextInputt  from "../components/TextInputt";
 import TextInputMask from 'react-native-text-input-mask';
 import InputMask from "../components/InputMask";
-
+import AutoTags from 'react-native-tag-autocomplete';
 
 const H = Dimensions.get('screen').height;
 const W = Dimensions.get('screen').width;
@@ -20,26 +20,28 @@ export default class Hizmet extends Component {
  
     this.state = {
       
-      ImageSource2:null,
+      
       ImageSource: null,
       data: null,
-      data2:null,
+      
+      diller:'',
+      tagsSelected: [],
 
-     iban:false,
-     paypal:false,
-     kredikarti:false,
-     bankahesabı:false, 
+      iban:false,
+      paypal:false,
+      kredikarti:false,
+      bankahesabı:false, 
      
 
       Adi: '',
       Soyadi: '',
-      Aranilan_dil: '',
+      Aranilandil: '',
       Telefon:'',
       Email:'',
       Password:'',
       İban:'',
       Paypal:'',
-      Kredi_Karti:'',
+      Kredi_karti:'',
       Banka_Hesabi:'',
       Onay_Kodu:''
  
@@ -117,19 +119,18 @@ export default class Hizmet extends Component {
   }
   uploadImageToServer = () => {
  
-    RNFetchBlob.fetch('POST', 'https://reactnativedeneme.000webhostapp.com/upload_image.php', {
+    RNFetchBlob.fetch('POST', 'http://192.168.8.137/User_Project/upload_image.php', {
       Authorization: "Bearer access-token",
       otherHeader: "foo",
       'Content-Type': 'multipart/form-data',
     }, [
-        { name: 'image', filename: 'image.png', type: 'image/png', data: this.state.data}
+        { name: 'image', filename: 'image.png', data: this.state.data}
       ]).then((resp) => {
  
         var tempMSG = resp.data;
-        
  
         tempMSG = tempMSG.replace(/^"|"$/g, '');
-       
+ 
         Alert.alert(
           'Alert Title',
           tempMSG,
@@ -143,7 +144,7 @@ export default class Hizmet extends Component {
             {text: 'OK', onPress: () => console.log('OK Pressed')},
           ],
           {cancelable: false},
-        );
+        );;
  
       }).catch((err) => {
         // ...
@@ -151,44 +152,11 @@ export default class Hizmet extends Component {
  
   }
 
-  uploadImageToServer2 = () => {
- 
-    RNFetchBlob.fetch('POST', 'https://reactnativedeneme.000webhostapp.com/upload_image.php', {
-      Authorization: "Bearer access-token",
-      otherHeader: "foo",
-      'Content-Type': 'multipart/form-data',
-    }, [
-        { name: 'image', filename: 'image.png', type: 'image/png', data2: this.state.data2 }
-      ]).then((resp) => {
- 
-     
-        var tempMSG2= resp.data2;
- 
-        tempMSG2= tempMSG2.replace(/^"|"$/g, '');
-        Alert.alert(
-          'Alert Title',
-          tempMSG2,
-          [
-            {text: 'Ask me later', onPress: () => console.log('Ask me later pressed')},
-            {
-              text: 'Cancel',
-              onPress: () => console.log('Cancel Pressed'),
-              style: 'cancel',
-            },
-            {text: 'OK', onPress: () => console.log('OK Pressed')},
-          ],
-          {cancelable: false},
-        );
- 
-      }).catch((err) => {
-        // ...
-      })
- 
-  }
 
-UserRegistrationFunction = () =>{
- 
-  fetch('https://reactnativedeneme.000webhostapp.com/user_registration.php', {
+
+ UserRegistrationFunction = () =>{
+
+  fetch('http://192.168.8.137/User_Project/hizmet.php', {
     method: 'POST',
     headers: {
       'Accept': 'application/json',
@@ -200,23 +168,24 @@ UserRegistrationFunction = () =>{
   
       soyadi: this.state.Soyadi,
   
-      aranilan_dil: this.state.Aranilan_dil,
+      aranilandil: this.state.Aranilandil,
 
       telefon: this.state.Telefon,
-  
+      
       email: this.state.Email,
-
+      
       password:this.state.Password,
-  
+      
       iban: this.state.İban,
-
+      
       paypal:this.state.Paypal,
-
+      
       kredikarti:this.state.Kredi_Karti,
-
+      
       bankahesabi:this.state.Banka_Hesabi,
-
-      onay: this.state.Onay_Kodu
+      
+      onaykodu: this.state.Onay_Kodu
+      
   
     })
   
@@ -242,7 +211,7 @@ UserRegistrationFunction = () =>{
         }).catch((error) => {
           console.error(error);
         });
- 
+      
 }
  
 validates = () => { 
@@ -262,30 +231,70 @@ validates = () => {
   } 
 
 }
+handleDelete = index => {
+  //tag deleted, remove from our tags array
+  let tagsSelected = this.state.tagsSelected;
+  tagsSelected.splice(index, 1);
+  this.setState({ tagsSelected });
+}
+
+handleAddition = dil => {
+  //suggestion clicked, push it to our tags array
+  this.setState({ tagsSelected: this.state.tagsSelected.concat([dil]) });
+}
+
+componentDidMount() {
+  return fetch('http://192.168.8.137/User_Project/diller.php')
+    .then((response) => response.json())
+    .then((responseJson) => {
+    this.setState({Aranilandil:responseJson})
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
 
 kayitol = () => {
-
+  const { Adi }  = this.state ;
+  const { Soyadi }  = this.state ;
+  const { Telefon }  = this.state ;
+  const { Aranilandil }  = this.state ;
+  const { Email }  = this.state ;
+  const { Password }  = this.state ;
+  
+if (Adi||Soyadi||Telefon||Aranilandil||Email||Password !='') {
+ this.UserRegistrationFunction&&this.uploadImageToServer
+} else {
+  Alert.alert('')
+ 
 }
+}
+
   render() {
     return (
       
       <SafeAreaView>
       <ScrollView >
- 
-      <TextInputt  
-      placeholder='Aranılan Dil' 
-      returnKeyType='go'
-      onChangeText={aranilan_dil => this.setState({Aranilan_dil : aranilan_dil})}
-      />
+      <View style={{alignItems:'center'}}>
+
+      <AutoTags
+            suggestions={this.state.Aranilandil}
+            tagsSelected={this.state.tagsSelected}
+            handleAddition={this.handleAddition}
+            handleDelete={this.handleDelete}
+          /> 
+
+          </View>
+      
     
       <TextInputt  
       placeholder='Adınız' 
-      onChangeText={adi => this.setState({Adi : adi})}
+      onChangeText={text => this.setState({Adi : text})}
       />
       
       <TextInputt 
       placeholder='Soyadınız' 
-      onChangeText={soyadi => this.setState({Soyadi : soyadi})}
+      onChangeText={text => this.setState({Soyadi : text})}
       />
      <TouchableOpacity onPress={this.selectPhotoTapped.bind(this)}>
           <View style={styles.ImageContainer}>
@@ -295,29 +304,22 @@ kayitol = () => {
             }
           </View>
       </TouchableOpacity>
-          
-      <TouchableOpacity onPress={this.selectPhotoTapped2.bind(this)}>
-          <View style={styles.ImageContainer}>
-
-            {this.state.ImageSource2 === null ? <Text>Kimliğinizi Yükleyin</Text> :
-              <Image style={styles.ImageContainer} source={this.state.ImageSource2} />
-            }
-
-          </View>
-      </TouchableOpacity>
+      <Button
+      
+      title="resimleri yükle" onPress={this.uploadImageToServer}/>   
      <TextInputt 
       placeholder='Emailiniz' 
       keyboardType='email-address'
-      onChangeText={email => this.setState({Email : email})}
+      onChangeText={text => this.setState({Email : text})}
       />
       <TextInputt 
       placeholder='Şifre oluşturun' 
-      onChangeText={password => this.setState({Password : password})}
+      onChangeText={text => this.setState({Password : text})}
       />
      <InputMask
         keyboardType='phone-pad'
         placeholder='Telefonunuz'   
-        onChangeText={telefon => this.setState({Telefon : telefon})}
+        onChangeText={text => this.setState({Telefon : text})}
         mask={"+90 ([000]) [000] [00] [00]"}
       />
 <Text style={{alignSelf:'center',margin:10}}>Ödeme Yöntemini Seçin</Text>
@@ -357,7 +359,7 @@ kayitol = () => {
         onChange={()=>{
           this.setState({bankahesabı:!this.state.bankahesabı,kredikarti:false,iban:false,paypal:false})
       }}
-      checked={this.state.bankahesabi}
+      checked={this.state.bankahesabı}
       label={"Banka Hesabı"}/>
     </View>
 </View>
@@ -372,10 +374,9 @@ kayitol = () => {
       />
       :this.state.paypal==true ? 
       <InputMask
-      keyboardType='phone-pad'
+      keyboardType='email-address'
       placeholder='Ödeme Alımı Paypal' 
       onChangeText={paypal => this.setState({Paypal: paypal})}
-      mask={"[0000] [0000] [0000] [0000]"}
       />:this.state.kredikarti==true ? 
       <InputMask 
       keyboardType='phone-pad'
@@ -403,7 +404,8 @@ kayitol = () => {
     
 
 
-      <Button title="Kayıt" onPress={this.UserRegistrationFunction&&this.uploadImageToServer&&this.uploadImageToServer2}/>
+      
+      <Button title="Kayıt" onPress={this.UserRegistrationFunction}/>
       </ScrollView>
       </SafeAreaView>
     )
